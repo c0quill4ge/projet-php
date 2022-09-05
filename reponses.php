abf6fbaf70400e246a17dcea6da982eb0d23d4d6
Victoria <vickydg2005@gmail.com>
2022-09-05 23:15:57 +0200

    Create reponses.php
    récupérer pour résumer les réponses du quiz

---
+++ reponses.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Résumé</title>
+</head>
+<body>
+
+Pour résumer voici tes réponses:</br>
+<?php
+
+echo "La solution pour"
