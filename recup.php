2be9aa53b2650738f613a1e62aa2fb8877d56188
Victoria <vickydg2005@gmail.com>
2022-09-05 11:36:58 +0200

    Premier commit

--- recup.php
+++ recup.php
@@ -8,7 +8,12 @@ session_start();
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
 
-    $_SESSION['']=
-
+    $_SESSION['nom']= $_POST['nom']
+    $_SESSION['prenom']= $_POST['prenom']
+    $_SESSION['age']= $_POST['age']
+    $_SESSION['ville']= $_POST['ville']
+    $_SESSION['appareil']= $_POST['appareil']
+    $_SESSION['interet']= $_POST['interet']
+    $_SESSION['couleur']= $_POST['couleur']
 ?>
 </body>
