a4cb71ff689c70113ad5b8fe41e370e06859936e
Victoria <vickydg2005@gmail.com>
2022-09-05 11:45:45 +0200

    Create quiz.php

---
+++ quiz.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+?>
+
+<! DOCTYPE html>
+<title> Le quiz le plus dur du monde! </title>
+<body>
+
+<form method="post" action="reponses.php">
+<label for="qun"> Quelle est la solution pour 1+1=x? </label>
