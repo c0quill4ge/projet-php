a18e80d3d915ae23a4632d98820a529108a58b30
Victoria <vickydg2005@gmail.com>
2022-09-07 08:51:53 +0200

    Create quiz.php

---
+++ quiz.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title> Le quiz le plus dur du monde! </title>
+<body>
+
+<form method="post" action="reponses.php">
+Quelle est la solution pour 1+1=x? </br>
+<select name="un_et_un" id="un_et_un">
+    <option value="neant"> Veuillez sélectionner une réponse. </option>
+    <option value="carotte"> carotte </option>
+    <option value="11"> 11</option>
+    <option value="1"> 1</option>
+    <option value="2">2</option>
+</select></br>
+</br>
+Quelle est la racine carrée de 4?</br>
+<select name="rac_car" id="rac_car">
+    <option value="neant">Veuillez selectionner une reponse.</option>
+    <option value="2">2</option>
+    <option value="8">8</option>
+    <option value="4">4</option>
+    <option value="16">16</option>
+</select></br>
+</br>
+Quel nom porte le fameux binôme?</br>
+<select name="bin" id="bin">
+    <option value="neant">Veuillez sélectionner une réponse.</option>
+    <option value="Euler"> Euler</option>
+    <option value="Newton"> Newton</option>
+    <option value="Gauss">Gauss</option>
+    <option value="Thalès">Pythagore</option>
+</select></br>
+</br>
+En mathématiques, un certain triangle est fort utile... lequel? Le triangle de:</br>
+<select name="tri" id="tri">
+    <option value="neant">Veuillez sélectionner une réponse.</option>
+    <option value="Patoche">Patoche</option>
+    <option value="Pythagore">Pythagore</option>
+    <option value="Pascal">Pascal</option>
+    <option value="Périclès">Périclès</option>
+</select></br>
+</br>
+<input type="submit" value="Envoyer" />
+</form>
+</body>
+</html>
