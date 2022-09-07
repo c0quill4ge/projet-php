ddfec3377aa2e91531efa6345cd9154d991900ff
Victoria <vickydg2005@gmail.com>
2022-09-07 08:42:18 +0200

    euh

---
+++ correction.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Résumé</title>
+</head>
+<body>
+
+Voici la correction:</br>
+</br>
+
+La solution pour 1+1=x est: 2.</br>
+La racine carrée de 4 est: 2.</br>
+Le nom du fameux binôme est le binôme de Newton.</br>
+Le triangle très utile en mathématiques est le triangle de Pascal.</br>
+
--- reponses.php
+++ reponses.php
@@ -11,6 +11,15 @@ session_start();
 <body>
 
 Pour résumer voici tes réponses:</br>
+</br>
 <?php
+echo "La solution pour 1+1=x est: ".$_POST['un_et_un']."</br>";
 
-echo "La solution pour";
+echo "La racine carrée de 4 est: ".$_POST['rac_car']."</br>";
+
+echo "Le nom du fameux binôme est le binôme de ".$_POST['bin']."</br>";
+
+echo "Le triangle très utile en mathématiques est le triangle de ".$_POST['tri']."</br>";
+?>
+
+<a href="correction.php">CLIQUE pour la correction</a>
