7005fe14e4737348588755e7cdf42c7b887c7959
Victoria <vickydg2005@gmail.com>
2022-09-07 09:18:10 +0200

    Update site_mar.php

--- site_mar.php
+++ site_mar.php
@@ -4,5 +4,13 @@
     <meta charset="utf-8">
     <title> BIENVENUE!!!!!!!!!!</title>
 <body>
-
-<a href=""> DESVOITUUUUUUUUUUUTES</a>
+<?php
+session_start();
+if (isset($_SESSION["interet"=='jeux_videos'])){
+    echo '<a href=""> "WOW The LASt OF US EN RéduCTION"</a>';
+    echo '<a href=""> .$_SESSION["prenom"].", LA PS6 déJà en VENTE, CLIQUE ICI"</a>';
+} else {
+    echo "je n'ai rien sur toi, mais je vais t'enregistrer";
+    $_SESSION["a"]= 1;
+}
+?>
